1f27fed6b3500e20ab78a6076d14bcfcf7d68dff
dimitermusic <dimiteryyordanov@gmail.com>
2021-10-22 14:16:36 -0700

    database built, server mostly built, and todos added

---
+++ seeds.sql
@@ -0,0 +1,1 @@
+-- TODO: create seeds to test if working
---
+++ server.js
@@ -0,0 +1,148 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const express = require("express");
+const app = express()
+const PORT = process.env.PORT || 3001
+
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'genova66',
+        database: 'employee_db',
+    },
+    console.log(`Connected to the employee_db database.`)
+);
+
+const main = () => {
+    inquirer
+        .prompt({
+            type: 'list',
+            message: 'What do you want to do?',
+            name: 'choice',
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
+        })
+        .then((data) => {
+            if (data.choice === 'view all departments') {
+                db.query('SELECT * FROM department', (err, data) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        console.log(data);
+                        main()
+                    }
+                });
+            } else if (data.choice === 'view all roles') {
+                db.query('SELECT * FROM role', (err, data) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        console.log(data);
+                        main()
+                    }
+                });
+            } else if (data.choice === 'view all employees') {
+                db.query('SELECT * FROM employee', (err, data) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        console.log(data);
+                        main();
+                    }
+                });
+            } else if (data.choice === 'add a department') {
+                inquirer
+                    .prompt(
+                        {
+                            type: 'input',
+                            message: "What is the new department's name?",
+                            name: 'depName'
+                        }
+                    )
+                    .then((ans) => {
+                        db.query("INSERT INTO department(name)VALUES(?)", [ans.depName], (err, data) => {
+                            if (err) {
+                                throw err
+                            } else {
+                                console.log(data);
+                                main();
+                            }
+                        });
+                    })
+            } else if (data.choice === 'add a role') {
+                inquirer
+                    .prompt(
+                        {
+                            type: 'input',
+                            message: "What is the new role's name?",
+                            name: 'roleName'
+                        }
+                    )
+                    .then((ans) => {
+                        db.query("INSERT INTO role(name)VALUES(?)", [ans.roleName], (err, data) => {
+                            if (err) {
+                                throw err
+                            } else {
+                                console.log(data);
+                                main();
+                            }
+                        });
+                    })
+            } else if (data.choice === 'add an employee') {
+                inquirer
+                .prompt(
+                    {
+                        type: 'input',
+                        message: "What is the new employee's name?",
+                        name: 'employeeName'
+                    }
+                )
+                .then((ans) => {
+                    db.query("INSERT INTO employee(name)VALUES(?)", [ans.employeeName], (err, data) => {
+                        if (err) {
+                            throw err
+                        } else {
+                            console.log(data);
+                            main();
+                        }
+                    });
+                })
+            }
+            else if (data.choice === 'update an employee role') {
+                inquirer
+                .prompt(
+                    {
+                        type: 'input',
+                        message: "What is the id of the employee you are updating?",
+                        name: 'updateEmpId'
+                    },
+                    {
+                        type: 'input',
+                        message: "What is the id of the updated role you would like to assign to the employee?",
+                        name: 'updateRoleId'
+                    }
+                )
+                .then((ans) => {
+                    // TODO: Find a way to change the existing role of an employee using sql commands
+                    db.query("INSERT INTO employee(name)VALUES(?)", [ans.updateEmpId, updateRoleId], (err, data) => {
+                        if (err) {
+                            throw err
+                        } else {
+                            console.log(data);
+                            main();
+                        }
+                    });
+                })
+            }
+            else {
+                console.log("goodbye")
+                db.end();
+            }
+        });
+}
+
+main();
